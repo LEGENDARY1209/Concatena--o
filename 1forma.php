83715ef61cae9881fd0b79e5823665a0a510ebe1
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-09-22 08:03:38 -0300

    Commit do Raí

---
+++ 1forma.php
@@ -0,0 +1,12 @@
+<?php
+
+    // Exemplo de concatenação;
+    $rua = "Rua do Senai, 1000";
+    $bairro = "Jd do aprendizado";
+    $cidade = "Santo Andre";
+    $cep = "03001-000";
+    $enderecocompleto = $rua. " - " .$bairro. " - " .$cidade. " - " .$cep;
+
+    print $enderecocompleto
+
+?>
---
+++ 2forma.php
@@ -0,0 +1,13 @@
+<?php
+    // Exemplo de concatenação
+    $rua = "Rua do Senai, 1000";
+    $bairro = "Jd do aprendizado"; 
+    $cidade = "Santo André";
+    $cep = "03001-000";
+    $enderecocompleto = "$rua - ";
+    $enderecocompleto .= "$bairro  - ";
+    $enderecocompleto  .= "$cidade  - ";
+    $enderecocompleto   .= "$cep";
+    print $enderecocompleto
+
+?>
